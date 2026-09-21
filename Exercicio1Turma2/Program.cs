1275e5e1fafdd58c109d0cb937ea7980ee9c36a8
eduardootresp <eduardo_tresp@live.com>
2026-09-21 17:50:24 -0300

    Adicionar arquivos de projeto.

---
+++ Exercicio1Turma2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Exercicio1Turma2
+{
+    using System;
+
+    public abstract class Figura
+    {
+        public abstract string cor { get; set; }
+        public abstract double Area();
+    }
+
+    public class Retangulo : Figura
+    {
+        public double lado1 { get; set; }
+        public double lado2 { get; set; }
+
+        public override string cor { get; set; }
+
+        public override double Area()
+        {
+            return lado1 * lado2;
+        }
+    }
+
+    public class Triangulo : Figura
+    {
+        public double baseTriangulo { get; set; }
+        public double alturaTriangulo { get; set; }
+
+        public override string cor { get; set; }
+
+        public override double Area()
+        {
+            return (baseTriangulo * alturaTriangulo) / 2.0;
+        }
+    }
+
+    public static class Teste
+    {
+        public static string DetalhesFigura(Figura figura)
+        {
+            return "Cor da figura: " + figura.cor +
+                   " - Área da figura: " + figura.Area();
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            Retangulo figura1 = new Retangulo();
+
+            figura1.cor = "Azul";
+            figura1.lado1 = 3.5;
+            figura1.lado2 = 2;
+
+            Console.WriteLine(Teste.DetalhesFigura(figura1));
+        }
+    }
+}

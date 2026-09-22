c112a77d3dbc4b6be13ab6ca1eca799f78eac804
eduardootresp <eduardo_tresp@live.com>
2026-09-22 07:28:26 -0300

    Alterações para o código como ficou no Everest

--- Exercicio1Turma2/exercicio1Abstract.cs
+++ Exercicio1Turma2/exercicio1Abstract.cs
@@ -6,8 +6,27 @@ using System.Threading.Tasks;
 
 namespace Exercicio1Turma2
 {
-    using System;
+    /*ENUNCIADO
+     * Classes abstratas são úteis para implementar comportamentos comuns a classes derivadas.
 
+    Elas podem conter propriedades e métodos abstratos ou não abstratos, mas não podem ser instanciadas e permitem novas implementações de seus métodos virtuais.
+
+    Crie uma classe abstrata chamada Figura. Nessa classe, nós teremos uma propriedade abstrata cor do tipo string e um método abstrato double area().
+
+    Em seguida, vamos criar a classe Retangulo que vai herdar a classe Figura e declarar dois atributos nela como double (lado1 e lado2). Vamos também sobrescrever o método double area(), onde colocaremos um return com a função lado 1 * lado 2, para obtermos a área.
+
+    Vamos criar uma classe Triangulo, que vai herdar a classe Figura, com os atributos base e altura e um método double area() que retorna base * altura.
+
+    Para finalizar, vamos criar uma classe estática chamada Teste com um método estático chamado DetalhesFigura que receberá um objeto do tipo Figura e retornará as informações sobre o objeto.
+
+    Exemplo:
+
+    Retangulo retangulo = new Retangulo();
+    retangulo.cor = "Azul";
+    retangulo.lado1 = 3.5;
+    retangulo.lado2 = 2;
+    Teste.DetalhesFigura(retangulo); //"Cor da figura: Azul - Área da figura: 7"
+     */
     public abstract class Figura
     {
         public abstract string cor { get; set; }
---
+++ Exercicio1Turma2/exercicio2Abstract.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio1Turma2
+{
+    /* ENUNCIADO
+     * Classes abstratas são úteis para implementar comportamentos comuns a classes derivadas. Elas podem conter propriedades e métodos abstratos ou não abstratos, mas não podem ser instanciadas e permitem novas implementações de métodos virtuais.
+
+    Nesse exercício, você deverá implementar uma classe abstrata com o nome EsporteBase que será a base para outras classes. Essa classe deverá conter os seguintes métodos e propriedades abstratas:
+
+    Propriedades:
+        QuantidadeJogadoresPorTime
+        TempoDeJogoEmMinutos  
+
+    Métodos:
+        IniciarJogo
+        FinalizarJogo
+        ExibirDescricao  
+
+    Implemente uma segunda classe que irá herdar de EsporteBase, chamada Futebol. Essa classe deverá implementar as propriedade e métodos da classe base.
+
+    Crie um construtor para essa classe definindo as propriedades:
+
+    QuantidadeJogadoresPorTime = 11
+    TempoDeJogoEmMinutos = 90  
+
+    Os métodos IniciarJogo e FinalizarJogo, devem retornar as frases "O jogo foi iniciado" e "O jogo foi finalizado", respectivamente.
+
+    O método ExibirDescricao deverá retornar a frase "O futebol é um esporte praticado por X jogadores em cada time durante Y minutos", onde as informações X e Y devem ser os valores das propriedades preenchidas no construtor.
+    */
+
+    public class Solution
+    {
+
+    }
+
+    public abstract class EsporteBase
+    {
+        public abstract int QuantidadeJogadoresPorTime {get; set;}
+        public abstract int TempoDeJogoEmMinutos { get; set;}
+
+        public abstract string IniciarJogo();
+        public abstract string FinalizarJogo();
+        public abstract string ExibirDescricao();
+    }
+
+    public class Futebol : EsporteBase
+    {
+        public override int QuantidadeJogadoresPorTime { get; set; }
+        public override int TempoDeJogoEmMinutos { get; set; }
+        public Futebol()
+        {
+            QuantidadeJogadoresPorTime = 11;
+            TempoDeJogoEmMinutos = 90;
+        }
+
+        public override string IniciarJogo() { return "O jogo foi iniciado"; }
+        public override string FinalizarJogo() { return "O jogo foi finalizado"; }
+
+        public override string ExibirDescricao()
+        {
+            return "O futebol é um esporte praticado por " +QuantidadeJogadoresPorTime+" jogadores em cada time durante "+TempoDeJogoEmMinutos+" minutos";
+        }
+
+    }
+}
+
+
